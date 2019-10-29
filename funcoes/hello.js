4fe2e2b2b359e4023d25b0a55e4025fa3a328c70
João José Bichara <52118350+JoaoJoseBichara@users.noreply.github.com>
2019-10-29 00:04:03 -0300

    Add files via upload

---
+++ funcoes/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Exemplo de uma Função Simples
+ * @author João José
+ */
+
+ function helloWorld(){
+     let nome
+     nome = prompt ('qual é o seu nome ?')
+     alert ('Hello ' + nome)
+ }
---
+++ vincularJS/documento.js
@@ -0,0 +1,6 @@
+/**
+ * Exemplo de vinculação de um documento JS no HTML
+ * @author João José
+ */
+
+ document.write('<h2>' + 'Hello World' + '</h2>')
