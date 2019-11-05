f93826d0cff668e2d77d5154ff0988258c917c0a
JoaoJoseBichara <52118350+JoaoJoseBichara@users.noreply.github.com>
2019-11-04 22:15:24 -0300

    Add files via upload

---
+++ dom/lampada.js
@@ -0,0 +1,12 @@
+/**
+ * Estudo do DOM
+ * Estudo da função com passagem de parametros
+ */
+
+ function ligar() {
+    document.getElementById('lamp').src = 'on.jpg'
+ }
+
+ function desligar() {
+    document.getElementById('lamp').src = 'off.jpg'
+ }
