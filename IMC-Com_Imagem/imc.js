1a6e9b107e61c23bad1e0da17d457d043dfafd34
JoaoJoseBichara <52118350+JoaoJoseBichara@users.noreply.github.com>
2019-11-11 19:25:49 -0300

    Add files via upload

--- IMC-Com_Imagem/imc.js
+++ IMC-Com_Imagem/imc.js
@@ -4,7 +4,7 @@
  */
 
  // Variáveis globais
- let imc, altura, peso;
+ let imc, altura, peso
 
  function calcular(){
     peso = Number(frmImc.txtPeso.value.replace(",","."))
@@ -12,19 +12,23 @@
     imc = peso / (altura*altura)
     frmImc.txtImc.value = imc.toFixed(2)
  
-  if (imc < 16.99){
+  if (imc < 17){
     document.getElementById('status').src='imc1.png'
- } else if (imc > 17 && 18.49) {
+ } else if (imc >= 17 && imc < 18.5){
     document.getElementById('status').src='imc2.png'
- } if (imc > 18.50 && 24.99) {
+ } if (imc > 18.50 && imc < 24.99){
     document.getElementById('status').src='imc3.png' 
- } else if (imc > 25 && 29.99) {
+ } else if (imc > 25 && imc < 29.99){
     document.getElementById('status').src='imc4.png'
- } if (imc > 30 && 34.99) {
+ } if (imc > 30 && imc < 34.99){
     document.getElementById('status').src='imc5.png'
- } else if (imc > 35 && 39.99) {
+ } else if (imc > 35 && imc < 39.99){
     document.getElementById('status').src='imc6.png'
- } if (imc > 40) {
+ } if (imc > 40){
     document.getElementById('status').src='imc7.png'
  }
+}
+
+function limpar(){
+   document.getElementById('status').src='imc.png'
 }
